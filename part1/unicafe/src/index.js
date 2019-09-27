6dff2c38c13c13135a1bbabe97ed9f103d6cc1a6
Gloria Moraga <silmeriel@gmail.com>
2019-09-27 18:23:08 -0300

    first part task unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -9,11 +17,37 @@ const App = () => {
 
   return (
     <div>
-      code here
+       <Display value={value} />
+       <Button handleClick={() => setToValue(0)} text="good" />
+       <Button handleClick={() => setToValue(0)} text="neutral" />
+       <Button handleClick={() => setToValue(0)} text="increment" />
     </div>
   )
 }
+// const Display = props => <div>{props.value}</div>
 
+// const Button = (props) => (
+//   <button onClick={props.handleClick}>
+//     {props.text}
+//   </button>
+// )
+
+// const App = props => {
+//   const [value, setValue] = useState(10)
+
+//   const setToValue = newValue => {
+//     setValue(newValue)
+//   }
+
+//   return (
+//     <div>
+//       <Display value={value} />
+//       <Button handleClick={() => setToValue(1000)} text="thousand" />
+//       <Button handleClick={() => setToValue(0)} text="reset" />
+//       <Button handleClick={() => setToValue(value + 1)} text="increment" />
+//     </div>
+//   )
+// }
 ReactDOM.render(<App />, 
   document.getElementById('root')
 )
